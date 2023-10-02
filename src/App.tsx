234ce572a862caf2e2988b107355c8f462d6adb1
nicolas antonini <121265107+Nic-Antonini@users.noreply.github.com>
2023-10-02 07:39:21 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as Components from './App.styles';
+
+const App = () => {
+  return (
+    <Components.Container>
+      <Components.Area>
+        <Components.Header>
+          Lista de tarefas
+        </Components.Header>
+      </Components.Area>
+    </Components.Container>
+  );
+}
+
+export default App;
