f7786d046b9d9d06316c4b3cc8b0a1341e930350
nicolas antonini <121265107+Nic-Antonini@users.noreply.github.com>
2023-10-02 09:48:01 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
+import React from 'react';
 import * as Components from './App.styles';
+import { Item } from "./types/item";
 
 const App = () => {
+
+  const [list, setList] = React.useState<Item[]>([
+    {id: 1, name: 'Ir até a padaria', done: false},
+    {id: 2, name: 'Comprar pão', done: false},
+    {id: 3, name: 'Retirar dinheiro', done: true}
+  ]);
+
   return (
     <Components.Container>
       <Components.Area>
         <Components.Header>
           Lista de tarefas
         </Components.Header>
+
+        {/*área para adicionar tarefas*/}
+        {list.map((item, index) => (
+          <div>{item.name}</div>
+        ))
+        
+        }
+
       </Components.Area>
     </Components.Container>
   );
